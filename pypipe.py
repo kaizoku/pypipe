0979e3450105d2cce27d3af691087825f0f54736
Josh <kaizoku@phear.cc>
2014-01-05 20:32:45 -0800

    add import for extra codecs

--- pypipe.py
+++ pypipe.py
@@ -4,6 +4,11 @@ import operator
 import codecs
 from functools import reduce
 
+try: ## Attempt hackercodecs import
+    import hackercodecs
+except ImportError as e:
+    pass
+
 def main():
     stdin = sys.stdin.read()
     stdout = sys.stdout
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 from setuptools import setup
 
-setup(name		    = 'pypipe',
-      version		= '0.1',
-      description	= 'Tests the given webserver for enabled HTTP methods.',
-      author		= 'kaizoku',
-      author_email	= 'kaizoku@phear.cc',
-      py_modules	= ['pypipe'],
-      license		= 'WTF',
-      url		    = 'http://github.com/kaizoku/pypipe',
+setup(name              = 'pypipe',
+      version           = '0.2',
+      description       = 'Makes inline python code easy in shells.',
+      author            = 'kaizoku',
+      author_email      = 'kaizoku@phear.cc',
+      py_modules        = ['pypipe'],
+      license           = 'WTF',
+      url= 'http://github.com/kaizoku/pypipe',
       entry_points  = {
           'console_scripts': ['pypipe = pypipe:main']
           },
