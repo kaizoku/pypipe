3fd0c744a874065484a202382603e6bd1b8f95fc
Josh <kaizoku@phear.cc>
2014-01-08 00:42:47 -0800

    Add -i command line option to import modules

--- pypipe.py
+++ pypipe.py
@@ -1,17 +1,24 @@
 import sys
 import string
 import operator
+import argparse
 import codecs
 from functools import reduce
 
-try: ## Attempt hackercodecs import
-    import hackercodecs
-except ImportError as e:
-    pass
-
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", action="append", dest="imports", help="module to import")
+    parser.add_argument("code", action="append", help="code to execute")
+    args = parser.parse_args()
+    if not args.code:
+        parser.error("Must include code to execute.")
+
+    ## load modules in imports
+    globals().update({i: __import__(i) for i in args.imports})
+
+    ## set some helpful variables
     stdin = sys.stdin.read()
     stdout = sys.stdout
 
-    code = reduce(operator.concat, sys.argv[1:])
+    code = reduce(operator.concat, args.code)
     sys.stdout.write(eval(code))
